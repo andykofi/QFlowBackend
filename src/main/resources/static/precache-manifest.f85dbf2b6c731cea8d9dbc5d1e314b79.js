3a64e2bac66755da673a722c9d397d4bf974dcdf
Andykofi <andykofi@btinternet.com>
2019-04-22 23:36:24 +0100

    React app

---
+++ src/main/resources/static/precache-manifest.f85dbf2b6c731cea8d9dbc5d1e314b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0626c4dbd644daf5b908",
+    "url": "/static/js/main.54867960.chunk.js"
+  },
+  {
+    "revision": "5318e76a955ef518d472",
+    "url": "/static/js/2.ed1f65fe.chunk.js"
+  },
+  {
+    "revision": "0626c4dbd644daf5b908",
+    "url": "/static/css/main.3dcd6804.chunk.css"
+  },
+  {
+    "revision": "5318e76a955ef518d472",
+    "url": "/static/css/2.98e8198c.chunk.css"
+  },
+  {
+    "revision": "44c31d55bfbbffacf9cf7099891df3c0",
+    "url": "/index.html"
+  }
+];
